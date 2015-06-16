5e8c69203f6da149d5e38308e340fbb095beb3b2
renatomaaliw3 <renatomaaliw3@yahoo.com>
2015-06-17 06:14:48 +0800

    modified main.js file

--- js/main.js
+++ js/main.js
@@ -8,11 +8,20 @@ $(document).ready(function() { //if the DOM is ready
 
 		var _this = $(this);
 		var label_for_value = $.trim(_this.attr('for').toLowerCase()); //get the label text
-		var tab_content = _this.parents('.tab-menu').siblings('li').find("li." + label_for_value); //find td that matches the label_text
+		var tab_content = _this.parents('.tab-menu').siblings('li').find("li." + label_for_value).addClass('animation-hingeUp'); //change tab-content animation here!
 		
-		tab_content.siblings('li').hide(); 
+		tab_content.siblings('li').hide();
 		tab_content.show();
 		
 	});
 
 });
+
+/* 
+
+	Recommended tab-content animations 
+
+	1. animation-hingeUp
+
+*/
+
