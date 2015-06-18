f37310f9f62994f7b3cb7f9f3cc0a816681e1476
renatomaaliw3 <renatomaaliw3@yahoo.com>
2015-06-18 17:08:27 +0800

    Added Tab-Marker Animation

--- js/main.js
+++ js/main.js
@@ -7,11 +7,16 @@ $(document).ready(function() { //if the DOM is ready
 	$('.tab-menu').on('click', '.tab-marker li label', function() {
 
 		var _this = $(this);
-		var label_for_value = $.trim(_this.attr('for').toLowerCase()); //get the label text
+
+		var label_for_value = $.trim(_this.attr('for').toLowerCase()); //label for="tab-1"
+		
+		var parent_li = _this.parent('li').addClass('animation-slideDown'); //parent li of <label>
+
 		var tab_content = _this.parents('.tab-menu').siblings('li').find("li." + label_for_value).addClass('animation-hingeUp'); //change tab-content animation here!
 		
-		tab_content.siblings('li').hide();
-		tab_content.show();
+		tab_content.siblings('li').hide(); //hide other siblings li of .tab-menu
+
+		tab_content.show(); //show assigned tab-content that matches the label for value
 		
 	});
 
